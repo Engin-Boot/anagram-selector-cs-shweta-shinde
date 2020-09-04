577a358e9739e4e8b352e3f6eb3a6690a4248d85
shweta-shinde <47037519+shweta-shinde@users.noreply.github.com>
2020-09-04 21:28:05 +0530

    Completed TestCases

--- Anagram/Anagram.cs
+++ Anagram/Anagram.cs
@@ -7,11 +7,28 @@ namespace Anagram
     {
         public bool WordPairIsAnagram(string word1, string word2) {
             //Insert the correct implementation here
+            char[] word1_array = word1.ToLower().ToCharArray();
+            char[] word2_array = word2.ToLower().ToCharArray();
+            Array.Sort(word1_array);
+            Array.Sort(word2_array);
+            string sortedWord1 = new string(word1_array);
+            string sortedWord2 = new string(word2_array);
+            if (sortedWord1 == sortedWord2)
+                return true;
             return false;
         }
         public List<string> SelectAnagrams(string word, List<string> candidates) {
             //Insert the correct implementation here
-            return candidates;
+
+            List<string> SelectedCandidates = new List<string>();
+            foreach(string candidate in candidates)
+            {
+                if (WordPairIsAnagram(word, candidate))
+                    SelectedCandidates.Add(candidate);
+            }
+
+
+            return SelectedCandidates;
         }
     }
 }
